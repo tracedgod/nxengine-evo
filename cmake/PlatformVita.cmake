5e6c6f0a8108fe5bfffdb48880b5f446db89895d
tracedgod <bowestrace@gmail.com>
2024-12-02 22:56:27 -0500

    update PlatformVita.cmake

--- cmake/PlatformVita.cmake
+++ cmake/PlatformVita.cmake
@@ -5,9 +5,10 @@ endif()
 include("$ENV{VITASDK}/share/vita.toolchain.cmake" REQUIRED)
 include("$ENV{VITASDK}/share/vita.cmake" REQUIRED)
 
-set(VITA_APP_NAME "NXENGINE-EVO")
-set(VITA_TITLEID  "NXEV00001")
-set(VITA_VERSION  "01.00")
+set(VITA_APP_NAME       "NXENGINE-EVO")
+set(VITA_TITLEID        "NXEV00001")
+set(VITA_VERSION        "01.00")
+set(VITA_MKSFOEX_FLAGS  "${VITA_MKSFOEX_FLAGS} -d PARENTAL_LEVEL=1")
 
 vita_create_self(${PROJECT_NAME}.self nx UNSAFE)
 vita_create_vpk(${PROJECT_NAME}.vpk ${VITA_TITLEID} ${PROJECT_NAME}.self
